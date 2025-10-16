228a99db9cb5cd2c724e8103ac6ff7ec42bbbfa2
Cristóbal Villalobos <cvillalobosn22@gmail.com>
2025-10-16 19:49:12 +0000

    chore: Remove tracked credentials and add .gitignore

--- set-admin-claim.js
+++
@@ -1,4 +0,0 @@
-// Este archivo ha sido vaciado intencionalmente.
-// No suba credenciales de servicio (serviceAccountKey.json) a su repositorio.
-// Consulte el archivo set-admin-claim.README.md para obtener instrucciones
-// sobre cómo manejar las credenciales de forma segura.
