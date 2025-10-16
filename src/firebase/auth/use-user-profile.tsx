88f06a66dd29b198e88302a7fbed11d5c8d070c5
Cristóbal Villalobos <cvillalobosn22@gmail.com>
2025-10-16 03:41:47 +0000

    Console Error
    Maximum update depth exceeded. This can happen when a co

--- src/firebase/auth/use-user-profile.tsx
+++ src/firebase/auth/use-user-profile.tsx
@@ -4,12 +4,15 @@ import { useDoc } from '../firestore/use-doc';
 import type { UserProfile } from '@/lib/types';
 import { useFirestore } from '../provider';
 import { doc } from 'firebase/firestore';
+import React from 'react';
 
 export function useUserProfile(uid: string | undefined) {
   const firestore = useFirestore();
 
-  const userProfileRef =
-    uid && firestore ? doc(firestore, 'users', uid) : null;
+  const userProfileRef = React.useMemo(() => {
+    if (!uid || !firestore) return null;
+    return doc(firestore, 'users', uid);
+  }, [uid, firestore]);
     
   const { data: userProfile, loading } = useDoc<UserProfile>(userProfileRef);
 
