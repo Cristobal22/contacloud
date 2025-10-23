5ef3a0bdec899afc6ec82be4722a98b37e112b8b
Cristóbal Villalobos <cvillalobosn22@gmail.com>
2025-10-23 18:02:15 +0000

    se ve demasiado grande en la franja superior de la pagina de inicio asi

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Link from 'next/link';
@@ -10,7 +11,7 @@ export function Header() {
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Logo variant="horizontal" />
+            <Logo variant="horizontal" className="h-10 w-auto" />
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
